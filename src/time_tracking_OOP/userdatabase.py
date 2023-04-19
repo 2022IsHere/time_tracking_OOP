4042bb4d4f1ae92d9a2a02823ef4f08586bece89
Uras Ayanoglu <uras@ayanoglu.org>
2023-04-20 00:26:18 +0300

    Add user_update method

--- src/time_tracking_OOP/userdatabase.py
+++ src/time_tracking_OOP/userdatabase.py
@@ -18,6 +18,7 @@ class UserDatabase:
                 (name TEXT, day DATE, work_time DATE, break_time DATE)''')
             connection.commit()
 
+
     def add_user(self, name, day, work_time, break_time):
         """Add a new user to the database"""
         with sqlite3.connect('userdata.db') as connection:
@@ -31,6 +32,7 @@ class UserDatabase:
             except:
                 pass
 
+
     def find_user(self, name):
         """Check if a specific user is in the database"""
 
@@ -43,6 +45,20 @@ class UserDatabase:
         if result:
             return result
         
+        
+    def find_date(self, day):
+        """Check if a specific date is in the database"""
+
+        with sqlite3.connect('userdata.db') as connection:
+            cursor = connection.cursor()
+            cursor.execute("SELECT * FROM users WHERE day=?", (day,))
+            # fetch the results and print them
+            result = cursor.fetchone()
+
+        if result:
+            return result
+        
+        
     def clean_database(self):
         '''function to swipe clean database when wanted to'''
 
@@ -50,6 +66,7 @@ class UserDatabase:
             cursor = connection.cursor()
             cursor.execute('''DROP TABLE IF EXISTS users''')
             connection.commit()
+
 
     def see_database(self):
         '''function to print out entire database'''
@@ -61,43 +78,12 @@ class UserDatabase:
             result = cursor.fetchall()
             print(result)
 
-    def add_work_time(self, work_time ,day, name):
-        '''function to add total work time'''
 
+    def update_user(self, name, day, work_time, break_time):
+        """Update a user's information in the database"""
         with sqlite3.connect('userdata.db') as connection:
             cursor = connection.cursor()
-            cursor.execute('''UPDATE users set work_time=?, day=? WHERE name=?''',(work_time,day,name))
+            cursor.execute('''UPDATE users SET work_time=?, break_time=? WHERE name=? AND day=?''',
+                        (work_time, break_time, name, day))
             connection.commit()
-
-    def add_break_time(self, break_time, day, name):
-        '''function to add total break time'''
-
-        with sqlite3.connect('userdata.db') as connection:
-            cursor = connection.cursor()
-            cursor.execute('''UPDATE users set break_time=?, day=? WHERE name=?''',(break_time,day,name))
-            connection.commit()
-
-
-#databaseObject = UserDatabase()
-#databaseObject.create_database()       
-
-
-# get player name + add player to database
-#user_name = input("Enter user name: ")
-#databaseObject.add_user(user_name, "18.04.2023", '00:00:00', '00:00:00')
-
-# see what's inside database
-#databaseObject.find_user(user_name)
-
-# print out entire database
-#databaseObject.see_database()
-
-# clear entire database
-#databaseObject.clean_database()
-
-# create database again
-#databaseObject.create_database()
-
-# print out again entire database
-#databaseObject.see_database()
 

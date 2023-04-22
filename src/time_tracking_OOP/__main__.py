3f3a8a203cd7ed54a794774823a06f80aed9a876
Uras Ayanoglu <uras@ayanoglu.org>
2023-04-22 16:07:34 +0300

    Modify __main__.py of the package

--- src/time_tracking_OOP/__main__.py
+++ src/time_tracking_OOP/__main__.py
@@ -1,4 +1,10 @@
-import time_tracking_OOP
+#import time_tracking_OOP
+from time_tracker import TimeTracker
+
+def main():
+    app = TimeTracker()
+    app.show_time()
+    app.mainloop()
 
 if __name__ == '__main__':
-    pass
+    main()
